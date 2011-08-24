c322aa385e69063f61d374b5af1f44cd576facef
Justin Windle <justin@soulwire.co.uk>
2011-08-24 07:01:50 +0100

    Added 'Darkness' render mode

--- js/recursion.js
+++ js/recursion.js
@@ -7,6 +7,13 @@
 
 var CONFIG  = {};
 var PRESETS = {};
+var RENDER_MODES = {
+	Segmented: 'segmented',
+	Sketched:  'sketched',
+	Darkness:  'darkness'
+};
+
+CONFIG.RENDER_MODE = RENDER_MODES.Darkness;
 
 PRESETS['Default'] = {
 
@@ -30,8 +37,10 @@ PRESETS['Default'] = {
 	MAX_DIVERGENCE:      0.05
 };
 
-PRESETS['Roots'] = {BRANCH_PROBABILITY:0.05,MAX_CONCURRENT:800,NUM_BRANCHES:3,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.28,MAX_WANDER_STEP:0.7000000000000001,MIN_GROWTH_RATE:5,MAX_GROWTH_RATE:9,MIN_SHRINK_RATE:0.98,MAX_SHRINK_RATE:0.99,MIN_DIVERGENCE:0.01,MAX_DIVERGENCE:0.05};
-PRESETS['Long'] = {BRANCH_PROBABILITY:1,MAX_CONCURRENT:350,NUM_BRANCHES:3,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.1,MAX_WANDER_STEP:0.72,MIN_GROWTH_RATE:0.9,MAX_GROWTH_RATE:6.15,MIN_SHRINK_RATE:0.935,MAX_SHRINK_RATE:0.999,MIN_DIVERGENCE:0.01,MAX_DIVERGENCE:0.05};
+PRESETS['Roots']     = {BRANCH_PROBABILITY:0.05,MAX_CONCURRENT:800,NUM_BRANCHES:3,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.28,MAX_WANDER_STEP:0.7000000000000001,MIN_GROWTH_RATE:5,MAX_GROWTH_RATE:9,MIN_SHRINK_RATE:0.98,MAX_SHRINK_RATE:0.99,MIN_DIVERGENCE:0.01,MAX_DIVERGENCE:0.05};
+PRESETS['Roots 2']   = {BRANCH_PROBABILITY:0.09,MAX_CONCURRENT:500,NUM_BRANCHES:5,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.1,MAX_WANDER_STEP:0.2,MIN_GROWTH_RATE:3.7,MAX_GROWTH_RATE:8,MIN_SHRINK_RATE:0.97,MAX_SHRINK_RATE:0.99,MIN_DIVERGENCE:0.01,MAX_DIVERGENCE:0.05};
+PRESETS['Long']      = {BRANCH_PROBABILITY:1,MAX_CONCURRENT:350,NUM_BRANCHES:3,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.1,MAX_WANDER_STEP:0.72,MIN_GROWTH_RATE:0.9,MAX_GROWTH_RATE:6.15,MIN_SHRINK_RATE:0.935,MAX_SHRINK_RATE:0.999,MIN_DIVERGENCE:0.01,MAX_DIVERGENCE:0.05};
+PRESETS['Tree']      = {BRANCH_PROBABILITY:0.06,MAX_CONCURRENT:437,NUM_BRANCHES:1,MIN_RADIUS:0.1,MAX_RADIUS:50,MIN_WANDER_STEP:0.05,MAX_WANDER_STEP:0.25,MIN_GROWTH_RATE:5,MAX_GROWTH_RATE:9,MIN_SHRINK_RATE:0.98,MAX_SHRINK_RATE:0.99,MIN_DIVERGENCE:0,MAX_DIVERGENCE:0.1};
 
 function configure(settings) {
 	for(var prop in settings) {
@@ -39,7 +48,7 @@ function configure(settings) {
 	}
 }
 
-configure(PRESETS['Default']);
+configure(PRESETS['Tree']);
 
 /**
  * --------------------
@@ -49,6 +58,7 @@ configure(PRESETS['Default']);
 
 var PI           = Math.PI;
 var TWO_PI       = Math.PI * 2;
+var HALF_PI      = Math.PI / 2;
 var BRANCHES     = [];
 
 function random(min, max) {
@@ -61,22 +71,27 @@ function random(min, max) {
  * --------------------
  */
 
-var Branch = function(x, y, theta, radius, scale) {
+var Branch = function(x, y, theta, radius, scale, generation) {
 
-	this.x       = x;
-	this.y       = y;
-	this.ox      = x;
-	this.oy      = y;
-	this.scale   = scale || 1.0;
-	this.theta   = theta;
-	this.radius  = radius;
-	this.growing = true;
+	this.x           = x;
+	this.y           = y;
+	this.ox          = x;
+	this.oy          = y;
+	this.x1          = NaN;
+	this.x2          = NaN;
+	this.y1          = NaN;
+	this.y2          = NaN;
+	this.scale       = scale || 1.0;
+	this.theta       = theta;
+	this.oTheta      = theta;
+	this.radius      = radius;
+	this.generation  = generation || 1;
+	this.growing     = true;
+	this.age         = 0;
 
 	this.wanderStep  = random(CONFIG.MIN_WANDER_STEP, CONFIG.MAX_WANDER_STEP);
 	this.growthRate  = random(CONFIG.MIN_GROWTH_RATE, CONFIG.MAX_GROWTH_RATE);
 	this.shrinkRate  = random(CONFIG.MIN_SHRINK_RATE, CONFIG.MAX_SHRINK_RATE);
-
-	this.children = [];
 }
 
 Branch.prototype = {
@@ -87,6 +102,7 @@ Branch.prototype = {
 			
 			this.ox = this.x;
 			this.oy = this.y;
+			this.oTheta = this.theta;
 
 			this.theta += random(-this.wanderStep, this.wanderStep);
 
@@ -104,6 +120,8 @@ Branch.prototype = {
 				var radius = this.radius * scale;
 				var branch = new Branch(this.x, this.y, theta, radius, scale);
 
+				branch.generation = this.generation + 1;
+
 				BRANCHES.push(branch);
 			}
 
@@ -111,6 +129,8 @@ Branch.prototype = {
 			if(this.radius * this.scale <= CONFIG.MIN_RADIUS) {
 				this.growing = false;
 			}
+
+			this.age++;
 		}
 	},
 
@@ -118,39 +138,154 @@ Branch.prototype = {
 
 		if(this.growing) {
 
-			var radius = this.radius * this.scale;
+			var x1, x2, y1, y2;
+			var scale = this.scale;
+			var radius = this.radius * scale;
 
 			context.save();
-			
-			// Draw outline
-			context.beginPath();
-			context.moveTo(this.ox, this.oy);
-			context.lineTo(this.x, this.y);
-			
-			if(radius > 5.0) {
-				context.shadowOffsetX = 2;
-				context.shadowOffsetY = 2;
-				context.shadowBlur    = 2 * this.scale;
-				context.shadowColor   = 'rgba(0,0,0,0.05)';	
+
+			switch(CONFIG.RENDER_MODE) {
+
+				case RENDER_MODES.Segmented :
+
+					// Draw outline
+					context.beginPath();
+					context.moveTo(this.ox, this.oy);
+					context.lineTo(this.x, this.y);
+					
+					if(radius > 5.0) {
+						context.shadowOffsetX = 2;
+						context.shadowOffsetY = 2;
+						context.shadowBlur    = 2 * scale;
+						context.shadowColor   = 'rgba(0,0,0,0.05)';	
+					}
+					
+					context.lineWidth = radius + scale;
+					context.strokeStyle = '#000';
+					context.lineCap = 'round';
+					context.stroke();
+					context.closePath();
+					
+					// Draw fill
+					context.beginPath();
+					context.moveTo(this.ox, this.oy);
+					context.lineTo(this.x, this.y);
+
+					context.lineWidth = radius;
+					context.strokeStyle = '#FFF';
+					context.lineCap = 'round';
+					context.stroke();
+
+					context.closePath();
+
+					break;
+				
+				case RENDER_MODES.Sketched :
+
+					x1 = this.x + Math.cos(this.theta - HALF_PI) * radius;
+					x2 = this.x + Math.cos(this.theta + HALF_PI) * radius;
+
+					y1 = this.y + Math.sin(this.theta - HALF_PI) * radius;
+					y2 = this.y + Math.sin(this.theta + HALF_PI) * radius;
+
+					context.lineWidth = 0.25 + scale;
+					context.strokeStyle = '#000';
+					context.fillStyle = '#FFF';
+					context.lineCap = 'round';
+					
+					// Starting point
+					if(this.generation === 1 && this.age === 1) {
+						context.beginPath();
+						context.arc(this.x, this.y, radius, 0, TWO_PI);
+						context.stroke();
+						context.fill();
+					}
+
+					// Draw sides
+					if(this.age > 1) {
+						context.beginPath();
+						context.moveTo(this.x1, this.y1);
+						context.lineTo(x1, y1);
+						context.moveTo(this.x2, this.y2);
+						context.lineTo(x2, y2);
+						context.stroke();
+					}
+
+					// Draw ribbon
+					context.beginPath();
+					context.moveTo(this.x1, this.y1);
+					context.lineTo(x1, y1);
+					context.lineTo(x2, y2);
+					context.lineTo(this.x2, this.y2);
+					context.closePath();
+					context.fill();
+
+					this.x1 = x1;
+					this.x2 = x2;
+
+					this.y1 = y1;
+					this.y2 = y2;
+
+					break;
+
+				case RENDER_MODES.Darkness :
+
+					x1 = this.x + Math.cos(this.theta - HALF_PI) * radius;
+					x2 = this.x + Math.cos(this.theta + HALF_PI) * radius;
+
+					y1 = this.y + Math.sin(this.theta - HALF_PI) * radius;
+					y2 = this.y + Math.sin(this.theta + HALF_PI) * radius;
+
+					context.lineWidth = scale;
+					context.strokeStyle = 'rgba(255,255,255,0.9)';
+					context.lineCap = 'round';
+					context.fillStyle = '#111';
+
+					// Starting point
+					if(this.generation === 1 && this.age === 1) {
+						context.beginPath();
+						context.arc(this.x, this.y, radius, 0, TWO_PI);
+						context.closePath();
+						context.fill();
+						context.stroke();
+					}
+					
+					// Shadow
+					if(scale > 0.05) {
+						context.shadowOffsetX = scale;
+						context.shadowOffsetY = scale;
+						context.shadowBlur    = scale;
+						context.shadowColor   = '#111';	
+					}	
+
+					// Draw ribbon
+					context.beginPath();
+					context.moveTo(this.x1, this.y1);
+					context.lineTo(x1, y1);
+					context.lineTo(x2, y2);
+					context.lineTo(this.x2, this.y2);
+					context.closePath();
+					context.fill();
+
+					// Draw sides
+					if(this.age > 1 && scale > 0.1) {
+						context.beginPath();
+						context.moveTo(this.x1, this.y1);
+						context.lineTo(x1, y1);
+						context.moveTo(this.x2, this.y2);
+						context.lineTo(x2, y2);
+						context.stroke();
+					}
+
+					this.x1 = x1;
+					this.x2 = x2;
+
+					this.y1 = y1;
+					this.y2 = y2;
+
+					break;
 			}
 			
-			context.lineWidth = radius + this.scale;
-			context.strokeStyle = '#000';
-			context.lineCap = 'round';
-			context.stroke();
-			context.closePath();
-			
-			// Draw fill
-			context.beginPath();
-			context.moveTo(this.ox, this.oy);
-			context.lineTo(this.x, this.y);
-
-			context.lineWidth = radius;
-			context.strokeStyle = '#FFF';
-			context.lineCap = 'round';
-			context.stroke();
-
-			context.closePath();
 			context.restore();
 		}
 	},
@@ -168,10 +303,11 @@ Branch.prototype = {
 
 var Recursion = new function() {
 
-	var started  = false;
-	var $canvas  = $('#canvas');
-	var canvas   = $canvas[0];
-	var context  = canvas.getContext('2d');
+	var started      = false;
+	var $canvas      = $('#canvas');
+	var $branchCount = $('#output .branchCount');
+	var canvas       = $canvas[0];
+	var context      = canvas.getContext('2d');
 
 	function spawn(x, y) {
 
@@ -202,6 +338,10 @@ var Recursion = new function() {
 				BRANCHES.splice(i,1);
 			}
 		}
+
+		var count = BRANCHES.length.toString();
+		while(count.length < 3) { count = '0' + count; }
+		$branchCount.text('Branch count: ' + count);
 	}
 
 	function onClick(e) {
@@ -283,7 +423,7 @@ var Recursion = new function() {
 function saveConfig() {
 	var config = [];
 	for(var i in CONFIG) { config.push(i + ':' + CONFIG[i]); }
-	console.log('{' + config.join(',') + '}');
+	console.log("PRESETS['__name__'] = {" + config.join(',') + "};");
 }
 
 // Build preset map for GUI
@@ -294,10 +434,10 @@ var GUI = new DAT.GUI({
 	width: 320
 });
 
-GUI.name('Settings');
+GUI.name('Recursion Settings');
 
 // Config
-GUI.add(CONFIG, 'NUM_BRANCHES').name('Branch Count').min(1).max(20).step(1);
+GUI.add(CONFIG, 'NUM_BRANCHES').name('Trunk Count').min(1).max(20).step(1);
 GUI.add(CONFIG, 'MAX_CONCURRENT').name('Max Concurrent').min(10).max(1000).step(1);
 GUI.add(CONFIG, 'BRANCH_PROBABILITY').name('Branch Probability').min(0.01).max(1.0).step(0.01);
 GUI.add(CONFIG, 'MIN_RADIUS').name('Radius (Min)').min(0.1).max(50);
@@ -308,6 +448,9 @@ GUI.add(CONFIG, 'MIN_GROWTH_RATE').name('Growth (Min)').min(0.1).max(20).step(0.
 GUI.add(CONFIG, 'MAX_GROWTH_RATE').name('Growth (Max)').min(0.1).max(20).step(0.1);
 GUI.add(CONFIG, 'MIN_SHRINK_RATE').name('Shrink (Min)').min(0.9).max(0.999).step(0.005);
 GUI.add(CONFIG, 'MAX_SHRINK_RATE').name('Shrink (Max)').min(0.9).max(0.999).step(0.005);
+GUI.add(CONFIG, 'MIN_DIVERGENCE').name('Divergeence (Min)').min(0.0).max(PI).step(0.01);
+GUI.add(CONFIG, 'MAX_DIVERGENCE').name('Divergeence (Max)').min(0.0).max(PI).step(0.01);
+GUI.add(CONFIG, 'RENDER_MODE').name('Render Mode').options(RENDER_MODES).onChange(Recursion.init);
 
 // Extra
 GUI.add(preset, 'key').name('Configuration').options(keys).onChange(function(){
@@ -318,8 +461,6 @@ GUI.add(preset, 'key').name('Configuration').options(keys).onChange(function(){
 GUI.add(Recursion, 'init').name('Restart Simulation');
 GUI.add(Recursion, 'save').name('Save Image');
 GUI.add(this, 'saveConfig').name('Save Settings');
-
-console.log(GUI.constrain);
 
 /**
  * --------------------
