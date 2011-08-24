7a21dd05b4c049e7b264680f51158528fc270152
Justin Windle <justin@soulwire.co.uk>
2011-08-24 12:19:16 +0100

    Added cancelRequestAnimFrame and licence

--- js/lib/RAF.js
+++ js/lib/RAF.js
@@ -1,11 +1,20 @@
-// shim layer with setTimeout fallback
+// Shim courtesy of Paul Irish & Jerome Etienne
+window.cancelRequestAnimFrame = ( function() {
+		return window.cancelAnimationFrame           ||
+				window.webkitCancelRequestAnimationFrame ||
+				window.mozCancelRequestAnimationFrame    ||
+				window.oCancelRequestAnimationFrame      ||
+				window.msCancelRequestAnimationFrame     ||
+				clearTimeout
+} )();
+
 window.requestAnimFrame = (function(){
-	return	window.requestAnimationFrame       || 
-					window.webkitRequestAnimationFrame || 
-					window.mozRequestAnimationFrame    || 
-					window.oRequestAnimationFrame      || 
-					window.msRequestAnimationFrame     || 
-					function(/* function */ callback, /* DOMElement */ element){
-						window.setTimeout(callback, 1000 / 60);
-					};
+		return  window.requestAnimationFrame   || 
+				window.webkitRequestAnimationFrame || 
+				window.mozRequestAnimationFrame    || 
+				window.oRequestAnimationFrame      || 
+				window.msRequestAnimationFrame     || 
+				function(/* function */ callback, /* DOMElement */ element){
+						return window.setTimeout(callback, 1000 / 60);
+				};
 })();
